15db843a99ccedac1163e1eef068510116650835
Melquisedeque <melquisedequevital@gmail.com>
2026-06-14 21:29:41 -0300

    feat: init spring bot in the project

---
+++ SisCom_Springboot/siscom/src/test/java/br/gov/siscom/SiscomApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.siscom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiscomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
